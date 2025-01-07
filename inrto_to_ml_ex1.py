9b1fecde6a17a1d14fad812a35568c792ab5f1ac
AmirAmber <67687509+AmirAmber@users.noreply.github.com>
2025-01-07 20:43:44 +0200

    backward pass needed

--- inrto_to_ml_ex1.py
+++ inrto_to_ml_ex1.py
@@ -1,8 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_breast_cancer
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+
 
 # Part 1: Single-Layer Neural Network with Gradient Descent
 class SingleLayerNN:
@@ -11,12 +10,22 @@ class SingleLayerNN:
         self.bias = np.zeros((1,))
         self.learning_rate = learning_rate
 
+
     def sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
+    def output(self, X):
+        w_1= np.random.rand(len(X), len(X))
+        z_1 = self.sigmoid(np.dot(X, w_1) + self.bias) # row len(m)
+        w_2 = np.random.rand(len(X), 1)
+        y_pred = self.sigmoid(np.dot(z_1, w_2) + self.bias) # scalar -> y_pred
+        return y_pred
+
+    def gradient_sigmoid(self, z):
+        return self.sigmoid(z) * (1 - self.sigmoid(z))
 
     def cross_entropy_loss(self, y_true, y_pred):
-        epsilon = 1e-10
-        return -np.mean(y_true * np.log(y_pred + epsilon) + (1 - y_true) * np.log(1 - y_pred + epsilon))
+        return -np.mean(y_true * np.log(y_pred ) + (1 - y_true) * np.log(1 - y_pred))  #loss function
+
 
     def train(self, X, y, epochs=1000, tol=1e-4):
         losses = []
